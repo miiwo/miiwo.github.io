e04cd909b9470ace8ff6ee48563a865e821da9fd
Jellyfishe <chelle.wong@mail.utoronto.ca>
2016-12-21 16:10:48 -0500

    Completed most of layout

--- app.js
+++ app.js
@@ -2,14 +2,13 @@ var main = function () {
     $('.buttonpush').click(function () {
         $('#Project').siblings().hide();
         
-        $('.containrect').animate({
-            width: "show"
-        }, 500);
-        
         $('.maincontent').animate({
             left: "+53%",
             width: "47%"
         }, 800, "swing", function () {
+            $('.containrect').animate({
+                height:'toggle'
+            }, 500);
             $('#Project').toggle('slow');
         });
         $('.maincontent').addClass("moved");
@@ -37,8 +36,28 @@ var main = function () {
         });
     });
     
-    
-
+    $('.dropdown-content').click(function (e) {
+        var name = e.target.textContent;
+        $('#' + name).siblings().hide();
+        
+        if (!$('.maincontent').hasClass('moved')) {
+            
+        
+            $('.maincontent').animate({
+                left: "+53%",
+                width: "47%"
+            }, 800, "swing", function () {
+                $('.containrect').animate({
+                    height: 'toggle'
+                }, 500);
+                $('#' + name).toggle('slow');
+            });
+            
+            $('.maincontent').addClass("moved");
+        } else {
+            $('#' + name).toggle('slow');
+        }
+    });
 };
 
 $(document).ready(main);
