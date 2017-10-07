14faf0b4fd2dfeca1213add4514148b61b3d15ca
Jellyfishe <chelle.wong@mail.utoronto.ca>
2017-10-07 18:13:10 -0400

    Polish up pages

--- app.js
+++ app.js
@@ -1,32 +1,40 @@
 var main = function () {
     $('.aboutbutton').click(function () {
-        $('#About').siblings().hide();
+        $('#About').siblings().hide(1000);
         
         if ($('.maincontent').hasClass('moved')) {
-            $('.picturecontainer').animate({
-                width: "hide"
-            }, 500);
+            $('#About').siblings().promise().done(function () {
+                $('.picturecontainer').animate({
+                    width: "hide"
+                }, 500);
             
-            $(".maincontent").removeClass("moved");
-            $(".maincontent").animate({
-                left: "0%",
-                width: "100%",
-                opacity: "1"
-            }, 800);
-            
+                $('.maincontent').removeClass('moved');
+                $('.maincontent').animate({
+                    left: "0%",
+                    width: "100%",
+                    opacity: "1"
+                }, 800);    
+            });
         }
-        $('.maincontent').promise().done(function () {
-            $('#About').toggle('slow');
+        
+        $('#About').siblings().promise().done(function () {
+            $('.maincontent').promise().done(function () {
+                $('#About').fadeIn(1000);
+            });
         });
     });
     
     $('.dropdown-content').click(function (e) {
-        var name = e.target.textContent;
-        $('#' + name).siblings().hide();
+        var name = '#' + e.target.textContent;
+        $(name).siblings().find('h2').hide();
+        $(name).siblings().find('p').hide();
         
-        if (!$('.maincontent').hasClass('moved')) {
+        
+        if (!$('.maincontent').hasClass('moved')) { // Initial State
             
-        
+            $('#Project').siblings().hide();
+            $('#Project').show();
+            
             $('.maincontent').animate({
                 left: "+53%",
                 width: "47%"
@@ -34,13 +42,20 @@ var main = function () {
                 $('.picturecontainer').animate({
                     height: 'toggle'
                 }, 500);
-                $('#' + name).toggle('slow');
+                
+                $(name + ' > h2').toggle('slow');
+                $(name + ' > h2').promise().done(function () {
+                    $(name + ' > p').fadeIn(1000);
+                });
             });
             
             $('.maincontent').addClass("moved");
-        } else {
-            if(!$('#' + name).is(":visible")){
-                $('#' + name).toggle('slow');
+        } else { //Moved State
+            if(!$(name + ' > h2').is(":visible")){
+                $(name + ' > h2').toggle('slow');
+                $(name + ' > h2').promise().done(function () {
+                    $(name + ' > p').fadeIn(1000);
+                });
             }
             
         }
