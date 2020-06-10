c3ae9e7a890e46243a6df20a6671d2effb9ea0e6
Michelle Wong <michellewong@Michelles-Air.local>
2020-06-10 11:34:01 -0400

    change basename on hashrouter

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
     };
     
     return (
-	<Router basename='/'>
+	<Router basename={process.env.PUBLIC_URL}>
 	    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
 		  integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk"
 		  crossorigin="anonymous" />
