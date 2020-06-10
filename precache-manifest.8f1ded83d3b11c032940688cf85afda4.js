1a6bc90b6595555ca117535c4c1633400e137195
Michelle Wong <michellewong@Michelles-Air.local>
2020-06-10 11:28:46 -0400

    Updates

---
+++ precache-manifest.8f1ded83d3b11c032940688cf85afda4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c44e72bdd8e3c2b7f5827c8bc01f7f99",
+    "url": "/jellyfishe.github.io/index.html"
+  },
+  {
+    "revision": "54a7051da916c134c1b3",
+    "url": "/jellyfishe.github.io/static/css/main.c2de3abe.chunk.css"
+  },
+  {
+    "revision": "7badcad9b744c8fae219",
+    "url": "/jellyfishe.github.io/static/js/2.4232b7a8.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/jellyfishe.github.io/static/js/2.4232b7a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54a7051da916c134c1b3",
+    "url": "/jellyfishe.github.io/static/js/main.5bef1376.chunk.js"
+  },
+  {
+    "revision": "9e1bbc8b450556d88504",
+    "url": "/jellyfishe.github.io/static/js/runtime-main.fc0df6f9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/jellyfishe.github.io/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f5dfb73eab072c66131c3d5ee1bbe56a",
+    "url": "/jellyfishe.github.io/static/media/profilepic.f5dfb73e.jpg"
+  },
+  {
+    "revision": "ee93708c25dd2d6288a550f3bf747636",
+    "url": "/jellyfishe.github.io/static/media/wave.ee93708c.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f57e55bbce17aef2665eb77c230b45e0.js"
+  "/jellyfishe.github.io/precache-manifest.8f1ded83d3b11c032940688cf85afda4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/jellyfishe.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
