5b1f818045197b950d745ac2cf1d710f43caad21
Michelle Wong <michellewong@Michelles-Air.local>
2020-08-25 15:52:40 -0400

    Updates

---
+++ precache-manifest.7cb2b225b12019d813b7df57cea8387f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "110f858bb51478aae4262cffd38a5187",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3baa6f5f359119ac4825",
+    "url": "/static/css/main.4b21be10.chunk.css"
+  },
+  {
+    "revision": "1efb1893b245aeb99321",
+    "url": "/static/js/2.7fa03072.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/static/js/2.7fa03072.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3baa6f5f359119ac4825",
+    "url": "/static/js/main.93ab18b4.chunk.js"
+  },
+  {
+    "revision": "2060b9a7e54a59ab7b07",
+    "url": "/static/js/runtime-main.f3a02c50.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f5dfb73eab072c66131c3d5ee1bbe56a",
+    "url": "/static/media/profilepic.f5dfb73e.jpg"
+  },
+  {
+    "revision": "ee93708c25dd2d6288a550f3bf747636",
+    "url": "/static/media/wave.ee93708c.svg"
+  }
+]);
