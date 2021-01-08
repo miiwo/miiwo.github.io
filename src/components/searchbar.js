709d4a7c800a0adadf4e66000016bd04d012d833
Michelle Wong <chellewsa@gmail.com>
2021-01-08 14:31:26 -0500

    rework searchbar functionality

--- src/components/searchbar.js
+++ src/components/searchbar.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import '../stylesheets/searchbar.css'
 
+const search_url = 'https://duckduckgo.com/?q=';
+
 function SearchSuggestionBox(props) {
-    const suggestionList = props.suggestions.length >= 3 ? props.suggestions.slice(0, 3).map((s, i) => <li key={i} className="search-suggestions">{s}</li>) : [];
+    const suggestionList = props.suggestions.length >= 3 ? props.suggestions.slice(0, 3).map((s, i) => 
+																<li key={i} className="search-suggestions">
+																	<a href={search_url+s}>{s}</a>
+																</li>) 
+														: [];
     return (
-	<ul id="search-suggestions">
-	    {suggestionList}
-	</ul>
+		<ul id="search-suggestions">
+			{suggestionList}
+		</ul>
     )
 }
 
@@ -15,6 +21,7 @@ class SearchBar extends React.Component {
 		super(props);
 		this.state = {value: '', suggestions: [], showSuggestions: true};
 		this.handleChange = this.handleChange.bind(this);
+		
     }
 
     handleChange(event) {
@@ -27,7 +34,7 @@ class SearchBar extends React.Component {
 	
 		const proxyurl = "https://cors-anywhere.herokuapp.com/";
 		fetch(proxyurl + 'https://duckduckgo.com/ac/?q=' + event.target.value)
-	    	.then(res => res.ok ? res.json() : console.log('Something went wrong'))
+	    	.then(res => res.ok ? res.json() : console.log('Something went wrong with trying to search from DuckDuckGo.'))
 	    	.then(data => {
 				console.log(data.map(d => d["phrase"]));
 				this.setState({ suggestions: data.map(d => d["phrase"]) })
@@ -39,7 +46,7 @@ class SearchBar extends React.Component {
     render() {
 		return (
 	    	<>
-				<form autoComplete="off" spellCheck="false" action="https://www.google.ca/search" method="get" target="_blank" id="search-form">
+				<form autoComplete="off" spellCheck="false" action="https://www.duckduckgo.com/" method="get" target="_blank" id="search-form">
 					<input name="q" type="text" value={this.state.value} onChange={this.handleChange} id="searchbar" placeholder="Type here to search for something." />
 					<SearchSuggestionBox suggestions={this.state.suggestions} />
 				</form>
