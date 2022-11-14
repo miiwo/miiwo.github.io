64889ba2ef44a338bc4aea8ebf6853a5dd690f62
koipoi <koipond575@gmail.com>
2022-11-13 19:51:12 -0500

    Update about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import info from '../information.json'
 import '../stylesheets/about.css'
+import seashell from '../res/seashell__30959.png'
 
 
 function About(props) {
     return (
 	<div className="about">
+		<img className='seashellPic' src={seashell} />
 	    <h1 className="display-4 aboutHeader">About</h1>
 	    <p className="aboutPara">{info.about}</p>
 	</div>	
