b120b55363bdafe957fe2af8692e0043f6433d77
koipoi <koipond575@gmail.com>
2022-12-22 00:58:00 -0500

    Add responsiveness to home page

--- src/components/topbar.js
+++ src/components/topbar.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 import '../stylesheets/topbar.css'
+import useWindowDimensions from './windowDimensions';
 
 
 function SidebarList(props) {
     const items = Object.keys(props.items);
+    const { height, width } = useWindowDimensions()
+    const mobileWidth = 365
+
     const sidebarItems = items.map((item, i) =>
-		<li key={i}><NavLink to={"/" + item} className="sidebar-link col-3">{item}</NavLink></li>
+    <>
+    	<li key={i}>
+        <NavLink to={"/" + item} className="sidebar-link col-3">{item}</NavLink>
+      </li>
+      { width < mobileWidth && <hr className='navHR' /> }
+    </>
     );
 
     return (
