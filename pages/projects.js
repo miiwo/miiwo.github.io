a8542ea51b6df6062105cc2542a3fad5115d4cdc
koipoi <koipond575@gmail.com>
2023-11-30 11:40:01 -0500

    Make project page responsive

--- pages/projects.js
+++ pages/projects.js
@@ -1,5 +1,7 @@
 import React from 'react'
+import Image from 'next/image'
 import info from '../src/information.json'
+import koi_yinyang from '../public/res/koibg.png'
 
 function ProjectHeader(props) {
     return (
@@ -15,12 +17,12 @@ function ProjectList(props) {
     const sideprojectItems = info.project_list.map((item, i) =>
 		<article className="entry" key={i}>
 			<div className="inner-entry">
-			<div id="icon"><i></i></div>
-			<h2 className="proj-title">
-				<a href={item.link}>{item.name}</a> {/*<span>{item.dev_cycle}</span> */}
-			</h2>
-			{/*<h4>Technologies used: </h4> */}
-			<p className="desc">{item.desc}</p>
+				<div id="icon"><i></i></div>
+				<h2 className="proj-title"><a href={item.link}>{item.name}</a> {/*<span>{item.dev_cycle}</span> */}</h2>
+				<p className="desc">{item.desc}</p>
+				<div>
+					
+				</div>
 			</div>
 		</article>
     );
@@ -30,7 +32,6 @@ function ProjectList(props) {
 	    <div className="projects-centered">
 			{sideprojectItems}
 	    </div>
-	    
 	</div>
     );
 }
@@ -40,6 +41,7 @@ function Projects(props) {
 	<div className="projects">
 	    <h1 className='display-4 projHeader'>Projects</h1>
 	    <ProjectList />
+		<Image src={koi_yinyang} width="10%" className='project-koi' />
 	</div>
 	
     );
